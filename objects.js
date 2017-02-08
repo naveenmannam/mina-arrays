7ac6c197aa2e0e0c4219deec01b34b30b6006edb
Naveen Mannam <naveen.mannam@hotmail.com>
2017-02-08 18:56:52 -0500

    added new object

--- objects.js
+++ objects.js
@@ -19,18 +19,29 @@ minny.dob = "10";
 minny.mob = "Jun";
 minny.yob = "1990";
 
-document.write(minny.firstName);
-document.write(</br>);
-document.write(minny.lastName);
-document.write(</br>);
-document.write(minny.dob);
-document.write(</br>);
-document.write(minny.mob);
-document.write(</br>);
-document.write(minny.yob);
 
-console.log(minny);
+var fname = document.getElementById('fname');
+var lname = document.getElementById('lname');
+var dob = document.getElementById('dob');
+var mob = document.getElementById('mob');
+var yob = document.getElementById('yob');
+var age = document.getElementById('yob');
 
-var minnyage = minny.age();
+fname.innerHTML = "First Name : <code> " + minny.firstName  + "</code>";
+lname.innerHTML = "Last Name : <code> " + minny.lastName  + "</code>";
+dob.innerHTML = "Date of Birth : <code> " + minny.dob  + "</code>";
+mob.innerHTML = "Month of Birth : <code> " + minny.mob  + "</code>";
+yob.innerHTML = "Year of Birth : <code> " + minny.yob  + "</code>";
+age.innerHTML = "Age : <code> " + minny.age()  + "</code>";
 
-console.log(minnyage);
+
+var sonu = new Object();
+
+// Another way of creating object in javascript.
+sonu.firstName = "Sonu";
+sonu.lastName = "Joel"
+sonu.age = 15;
+
+console.log(sonu.firstName);
+console.log(sonu.lastName);
+console.log(sonu.age);
