3374f46b07f2b5fd97bdaf4a030d72202e1f98c9
Naveen Mannam <naveen.mannam@hotmail.com>
2017-02-07 22:43:22 -0500

    Added new functionality in JS

--- app.js
+++ app.js
@@ -10,6 +10,28 @@ mypush.innerHTML = "value  has been added : " + ages.push(45);
 document.getElementById('my-after').innerHTML = "Values in the array after running the pop function : " + ages;
 
 var mypop = document.getElementById('my-pop');
-mypop.innerHTML = "value has been deleted : " + ages.pop();
+mypop.innerHTML = "Last value has been deleted : " + ages.pop();
 
 document.getElementById('my-final').innerHTML = "Values in the array after running the pop function : " + ages;
+
+var myshift = document.getElementById('my-shift');
+myshift.innerHTML = "First value has been deleted : " + ages.shift();
+
+document.getElementById('my-shifted').innerHTML = "Values in the array after running the pop function : " + ages;
+
+
+var getTotal = function(myvalue){
+	var sum = 0;
+	len = myvalue.length;
+	console.log(len);
+	for (i = 0; i < len; i++){
+		sum = sum + myvalue[i];
+	}
+	return sum;
+
+}
+
+
+var sumOfAges =getTotal(ages);
+
+console.log(sumOfAges);
